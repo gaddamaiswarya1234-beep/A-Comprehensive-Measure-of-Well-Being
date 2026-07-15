05a6240bb7548ee102ddb35dcb382076813a46d7
gaddamaiswarya1234-beep <gaddamaiswarya1234@gmail.com>
2026-07-15 17:29:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html")   # ✅ IMPORTANT
 
 @app.route('/predict', methods=['POST'])
 def predict():
