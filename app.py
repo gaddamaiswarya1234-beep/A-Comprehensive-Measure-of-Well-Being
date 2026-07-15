28237c97ca41bc2f36d77a4d1f4d86a16fbb5209
gaddamaiswarya1234-beep <gaddamaiswarya1234@gmail.com>
2026-07-15 17:23:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -8,15 +9,13 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get values from form
     life = float(request.form['life'])
     edu = float(request.form['edu'])
     income = float(request.form['income'])
 
-    # Simple formula (example)
     hdi = (life + edu + income) / 3
 
-    return render_template("index.html", prediction_text=f"HDI Value: {hdi:.2f}")
+    return render_template("index.html", prediction_text=f"HDI: {hdi:.2f}")
 
 if __name__ == "__main__":
     app.run()
